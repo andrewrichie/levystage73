0521ae1078a7f48865f5dbc9b9725a4674e660fe
andrewrichie <137726860+andrewrichie@users.noreply.github.com>
2025-04-13 16:25:41 +0100

    update90

--- script.js
+++ script.js
@@ -238,3 +238,13 @@ window.addEventListener("scroll", function () {
 
 
 
+
+//   navigation
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('show');
+});
