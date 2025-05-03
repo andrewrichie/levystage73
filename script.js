31f56e8e9d56db93fb8d9a1424831c140b93cc3e
andrewrichie <137726860+andrewrichie@users.noreply.github.com>
2025-05-03 18:10:02 +0100

    update 105

--- script.js
+++ script.js
@@ -182,21 +182,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-window.addEventListener("scroll", function () {
-    let line = document.querySelector(".toc-line");
-    let scrollPosition = window.scrollY; // Current scroll position
-    let maxScroll = document.documentElement.scrollHeight - window.innerHeight;
-    let scrollPercent = scrollPosition / maxScroll; // Scroll progress (0 to 1)
+// window.addEventListener("scroll", function () {
+//     let line = document.querySelector(".toc-line");
+//     let scrollPosition = window.scrollY; // Current scroll position
+//     let maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+//     let scrollPercent = scrollPosition / maxScroll; // Scroll progress (0 to 1)
 
-    // Define color transitions
-    if (scrollPercent < 0.33) {
-    line.style.background = "#03712E"; // Top color
-    } else if (scrollPercent < 0.66) {
-    line.style.background = "#0466D6"; // Middle color
-    } else {
-    line.style.background = "#C38C01"; // Bottom color
-    }
-});
+//     // Define color transitions
+//     if (scrollPercent < 0.33) {
+//     line.style.background = "#03712E"; // Top color
+//     } else if (scrollPercent < 0.66) {
+//     line.style.background = "#0466D6"; // Middle color
+//     } else {
+//     line.style.background = "#C38C01"; // Bottom color
+//     }
+// });
 
 
 
